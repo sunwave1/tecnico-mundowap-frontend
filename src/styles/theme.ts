d34568ffe9acbc9b0be18767ffe275a525eca3e1
sunwave1 <fellipeanthony1@hotmail.com>
2025-10-30 22:47:26 -0300

    feat: theme, add package hook forms, styled components and zustand

---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+    colors: {
+        primary: "#FFFFFF",
+        secondary: "#E11138",
+        success: "#008E5A",
+        error: "",
+        warning: "",
+        info: "",
+        light: "",
+        dark: "",
+    },
+    breakpoints: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+    },
+    spacing: {
+        xs: "8px",
+        sm: "16px",
+        md: "24px",
+        lg: "32px",
+        xl: "40px",
+    },
+    "border-radius": {
+        xs: "4px",
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "20px",
+    },
+};
