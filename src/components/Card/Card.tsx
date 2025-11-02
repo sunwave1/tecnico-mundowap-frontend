c033587569b5d374a78ff97f859b0f886491a87d
sunwave1 <fellipeanthony1@hotmail.com>
2025-11-01 21:19:55 -0300

    feat: card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,42 @@
+import {
+    MarginTypes,
+    PaddingTypes,
+    RadiusTypes,
+    SpacingTypes,
+    VariantTypes,
+} from "../../styles/theme";
+import { CardStyle } from "./styled";
+
+interface CardProps extends React.AllHTMLAttributes<HTMLDivElement> {
+    variant?: VariantTypes;
+    hasShadow?: boolean;
+    hasBorder?: boolean;
+    margin?: SpacingTypes | MarginTypes;
+    spacing?: SpacingTypes | PaddingTypes;
+    rounded?: RadiusTypes;
+}
+
+export default function Card({
+    children,
+    variant = "primary",
+    hasShadow = false,
+    hasBorder = false,
+    margin,
+    spacing = "sm",
+    rounded = "md",
+    ...props
+}: CardProps) {
+    return (
+        <CardStyle
+            variant={variant}
+            hasShadow={hasShadow}
+            hasBorder={hasBorder}
+            margin={margin}
+            spacing={spacing}
+            rounded={rounded}
+            {...props}
+        >
+            {children}
+        </CardStyle>
+    );
+}
