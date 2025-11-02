d5779b30b42a6cba29d59c6c812fe228d9c87b98
sunwave1 <fellipeanthony1@hotmail.com>
2025-11-02 15:23:52 -0300

    feat: main styled

---
+++ src/components/Main/styled.ts
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+import { MarginTypes, SpacingTypes } from "../../styles/theme";
+import { makeMargin, spacingStyle } from "../../styles/variants";
+
+interface MainProps {
+    $margin?: SpacingTypes | MarginTypes;
+}
+
+export const Main = styled.main<MainProps>`
+    ${({ $margin }) =>
+        typeof $margin === "object"
+            ? makeMargin($margin)
+            : `margin: ${spacingStyle($margin)};`}
+`;
